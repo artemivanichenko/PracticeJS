1c7a32b2e3a208ade7e192532b068923160f36bf
GoItTutor <114779287+GoItTutor@users.noreply.github.com>
2023-01-22 23:41:35 +0200

    Initialize module-01-lesson-01

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/*
+ * Базовые примитивные типы: number, bigInt, symbol, string, boolean, null и undefined.
+   Оператор typeof
+ */
+
+/*
+ * Вывод данных: console.log и alert. Вкладка Console в Chrome Devtools.
+ */
+
+// console.log('10');
+// alert('Hello');
+
+/*
+ * Переменные: объявление с let и const, разница, именование.
+ */
+
+/*
+ * Получение данных: prompt и confirm.
+ */
+
+// const userAge = prompt('Сколько тебе лет?');
+// const isAdult = confirm('Вам есть 18 лет?');
+
+// console.log(isAdult);
